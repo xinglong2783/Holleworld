6b910f5f960c92fd97ecb93c91d0213692faa20c
admin <2783599597@qq.com>
2021-08-19 20:18:19 +0800

    Initial commit

---
+++ src/test/java/com/example/holleworld/HolleworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holleworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolleworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
